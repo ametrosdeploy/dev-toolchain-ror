370459c838795f751e08b09b363a0cb3bd4e3587
sachin prasad <sachin.prasad@kiwitech.com>
2020-04-27 14:32:46 +0530

    - world serializer

---
+++ app/serializers/world_serializer.rb
@@ -0,0 +1,14 @@
+class WorldSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :customer_id, :world_code, :is_private
+  include DateHelper
+
+  attribute :created_on do |world|
+    format_to_ymd(world.created_at)
+  end
+
+  attribute :customer_name do |world|
+    world.customer.try(:name)
+  end
+
+end
