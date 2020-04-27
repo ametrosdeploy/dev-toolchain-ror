2d1f3b8f50f0c8ac84d12e7531dd5b6871239c04
sachin prasad <sachin.prasad@kiwitech.com>
2020-04-27 14:34:21 +0530

    - adding character & Customer serializer

---
+++ app/serializers/character_serializer.rb
@@ -0,0 +1,17 @@
+class CharacterSerializer
+  include FastJsonapi::ObjectSerializer
+  include ImageHelper
+  include DateHelper
+
+  attributes :full_name, :gender, :real_world
+
+  attribute :photo_url do |character|
+    image_url(character.photo)
+  end
+
+  attribute :age, &:formatted_age
+  
+  attribute :created_on do |character|
+    format_to_ymd(character.created_at)
+  end
+end
