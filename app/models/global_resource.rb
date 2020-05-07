89e999a547a8ca3745f1099344a45bc534645efa
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-08 00:24:48 +0530

    - Working on content management

--- app/models/global_resource.rb
+++ app/models/global_resource.rb
@@ -2,9 +2,16 @@ class GlobalResource < ApplicationRecord
   PER_PAGE = 10
   acts_as_ordered_taggable
 
-  belongs_to :customer
   enum resource_type: [:image, :document]
 
+  belongs_to :customer
+
+  has_many :world_global_resources
+  has_many :worlds, through: :world_global_resources
+
   has_one_attached :attachment
+
+  validates :customer_id, presence: true, if: :private?
+  validates :title, presence: true
 
 end
