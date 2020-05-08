dfe2eb5847e4a34845a3fd8c2c4830dada5a0f1f
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-08 18:38:43 +0530

    - Skip selected characters

--- app/models/organization_character.rb
+++ app/models/organization_character.rb
@@ -2,4 +2,8 @@ class OrganizationCharacter < ApplicationRecord
   belongs_to :character
   belongs_to :organization
   belongs_to :world_role
+
+  scope :limited_org_chars, -> (car_list) { where("character_id not in (?)",
+                                                                     car_list) }
+
 end
