6297b8bde2d87f616ba0a97fc1f708111ada47ad
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-12 01:26:35 +0530

    - Conflict

--- app/models/global_resource.rb
+++ app/models/global_resource.rb
@@ -2,47 +2,9 @@ class GlobalResource < ApplicationRecord
   PER_PAGE = 10
   acts_as_ordered_taggable
 
+  belongs_to :customer
   enum resource_type: [:image, :document]
 
-  belongs_to :customer, optional: true
-
-  has_many :world_global_resources
-  has_many :worlds, through: :world_global_resources
-
   has_one_attached :attachment
-
-  validates :customer_id, presence: true, if: :private?
-  validates :resource_type, :attachment, presence: true
-  validate :validate_attachment
-
-  scope :with_image, -> { where(resource_type:
-                                     GlobalResource::resource_types['image'] ) }
-  scope :with_document, -> { where(resource_type:
-                                   GlobalResource::resource_types['document']) }
-
-  # Validate attachment type
-  def validate_attachment
-    if attachment.attached? && image? && !valid_image?
-      errors.add(:document, 'Must be a jpg, jpeg or png')
-    elsif attachment.attached? && document? && !valid_document?
-      errors.add(:document, 'Must be a PDF or a DOC file')
-    end
-  end
-
-  # Used for searching Global Resource
-  def self.search keyword
-    where("active_storage_blobs.filename ilike :search or LOWER(cached_tag_list)
-     ILIKE :search", search: "%#{keyword.downcase}%")
-  end
-
-  private
-
-  def valid_document?
-    attachment.content_type.in?(%w(application/docx application/doc application/pdf))
-  end
-
-  def valid_image?
-    attachment.content_type.in?(%w(image/jpeg image/jpg image/png))
-  end
 
 end
