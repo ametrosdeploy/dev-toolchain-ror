891fcf5b9637d6ece08712c10457699390c39bbf
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-12 01:26:35 +0530

    - Conflict

--- app/serializers/global_resource_serializer.rb
+++ app/serializers/global_resource_serializer.rb
@@ -1,31 +1,4 @@
 class GlobalResourceSerializer
   include FastJsonapi::ObjectSerializer
-  include ImageHelper
-  include DateHelper
-
-  attributes :title, :description, :resource_type, :private, :tag_list
-
-  attribute :resource_url do |global_resource|
-    image_url(global_resource.attachment)
-  end
-
-  attribute :created_on do |global_resource|
-    format_to_ymd(global_resource.created_at)
-  end
-
-  attribute :customer do |global_resource|
-    CustomerSerializer.new(global_resource.customer).as_json["data"]
-  end
-
-  attribute :resource_size do |global_resource|
-    to_kb(global_resource.attachment_attachment.blob.byte_size)
-  end
-
-  attribute :file_name do |global_resource|
-    global_resource.attachment_attachment.blob.filename
-  end
-
-  attribute :tag_list do |global_resource|
-    global_resource.cached_tag_list
-  end
+  attributes :title, :description, :resource_type, :customer_id, :private
 end
