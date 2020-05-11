f9c3dd773aa759819ce92cbe97b974c9d13b9aa5
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-12 01:51:03 +0530

    - confllict

--- app/controllers/api/admin/v1/global_videos_controller.rb
+++ app/controllers/api/admin/v1/global_videos_controller.rb
@@ -1,14 +1,12 @@
-class Api::Admin::V1::GlobalVideosController < Api::Admin::V1::BaseController
+class Api::Admin::V1::GlobalVideosController < ApplicationController
   before_action :authenticate_user!
   before_action :set_global_video, only: [:show, :update, :destroy]
 
   def index
-    @global_videos = GlobalVideo.includes(:customer)
+    @global_videos = GlobalVideo.all
     @global_videos = @global_videos.search(params[:search]) if params[:search].present?
-    @global_videos = @global_videos.paginate(page:     params[:page],
-                                             per_page: GlobalVideo::PER_PAGE)
-    render json: serialize_rec(@global_videos).merge!(
-                       pagination_without_sort_hsh(@global_videos, GlobalVideo))
+    @global_videos = @global_videos.paginate(page: params[:page], per_page: GlobalVideo::PER_PAGE)
+    render json: serialize_rec(@global_videos).merge!(pagination_without_sort_hsh(@global_videos, GlobalVideo))
   end
 
   def show
@@ -44,23 +42,19 @@ class Api::Admin::V1::GlobalVideosController < Api::Admin::V1::BaseController
     param :header, :Authorization, :string, :required, 'Authorization'
     param :query, 'page', :string, :optional, 'Page Number'
     param :query, 'search', :string, :optional, 'Search Parameter'
+    param :query, 'sort_column', :string, :optional, 'Options: "name", "created_at",
+     "is_private", "learn_mods_count"'
+    param :query, 'sort_order', :string, :optional, 'Options: "asc", "desc"'
   end
 
   swagger_api :create do
     summary 'Creates a new global video'
     notes 'Should be used to create global video'
     param :header, :Authorization, :string, :required, 'Authorization'
-    param :form, 'global_video[title]', :string, :required, 'title'
-    param :form, 'global_video[description]', :string, :optional, 'description'
-    param :form, 'global_video[wistia_code]', :string, :required, 'wistia_code'
-    param :form, 'global_video[duration]', :integer, :required, 'duration'
-    param :form, 'global_video[video_type]', :string, :required, 'video_type Options:
-     "content", "plot_point", "module_intro"'
-    param :form, 'global_video[private]', :boolean, :optional, 'private'
-    param :form, 'global_video[transcript]', :string, :optional, 'transcript'
-    param :form, 'global_video[customer_id]', :integer, :optional, 'customer_id'
-    param :form, 'global_video[wistia_thumbnail]', :string, :optional, 'wistia_thumbnail'
-    param :form, 'global_video[tag_list]', :string, :optional, 'tag_list(Comma seperated)'
+    param :form, 'global video[name]', :string, :required, 'name'
+    param :form, 'global video[description]', :string, :optional, 'description'
+    param :form, 'global video[customer_id]', :integer, :optional, 'customer_id'
+    param :form, 'global video[is_private]', :boolean, :required, 'is_private'
     response :unauthorized
   end
 
@@ -80,18 +74,16 @@ class Api::Admin::V1::GlobalVideosController < Api::Admin::V1::BaseController
     param :form, 'global_video[description]', :string, :optional, 'description'
     param :form, 'global_video[wistia_code]', :string, :required, 'wistia_code'
     param :form, 'global_video[duration]', :integer, :required, 'duration'
-    param :form, 'global_video[video_type]', :string, :required, 'video_type Options:
-     "content", "plot_point", "module_intro"'
-    param :form, 'global_video[private]', :boolean, :optional, 'private'
-    param :form, 'global_video[transcript]', :string, :optional, 'transcript'
-    param :form, 'global_video[customer_id]', :integer, :optional, 'customer_id'
-    param :form, 'global_video[wistia_thumbnail]', :string, :optional, 'wistia_thumbnail'
-    param :form, 'global_video[tag_list]', :string, :optional, 'tag_list(Comma seperated)'
+    param :form, 'global_video[video_type]', :integer, :required, 'video_type'
+    param :form, 'global_video[private]', :integer, :required, 'private'
+    param :form, 'global_video[transcript]', :integer, :required, 'transcript'
+    param :form, 'global_video[customer_id]', :integer, :required, 'customer_id'
+    param :form, 'global_video[wistia_thumbnail]', :string, :required, 'wistia_thumbnail'
     response :unauthorized
   end
 
   swagger_api :destroy do
-    summary 'Destroy a global video'
+    summary 'Destroys a global video'
     notes 'Should be used to destroy a global video'
     param :header, :Authorization, :string, :required, 'Authorization'
     param :path, 'id', :string, :required, 'global video Id'
@@ -106,7 +98,7 @@ class Api::Admin::V1::GlobalVideosController < Api::Admin::V1::BaseController
     # Only allow a trusted parameter "white list" through.
     def global_video_params
       params.require(:global_video).permit(:title, :description, :wistia_code, :duration, 
-        :video_type, :private, :transcript, :wistia_thumbnail, :customer_id, :tag_list)
+        :video_type, :private, :transcript, :wistia_thumbnail)
     end
 
     def serializer
--- app/models/global_video.rb
+++ app/models/global_video.rb
@@ -1,25 +1,25 @@
 class GlobalVideo < ApplicationRecord
   PER_PAGE = 10
-  acts_as_ordered_taggable
+  acts_as_taggable
   enum video_type: [:content, :plot_point, :module_intro]
 
-  belongs_to :customer, optional: true
+  belongs_to :customer
 
   has_many :world_videos
   has_many :worlds, through: :world_videos
 
   has_many :video_learn_objs
-  has_many :learn_mods, through: :video_learn_objs
+  has_many :learn_mods through: :video_learn_objs
   
   has_one_attached :wistia_thumbnail
 
   validates :customer_id, presence: true, if: :private?
-  validates :title, :wistia_code, :duration, :video_type,  presence: true
+  validates :title, presence: true
 
   # Used for searching Global Videos
   def self.search keyword
-    where("LOWER(cached_tag_list) ILIKE :search or title ilike :search or 
-      transcript ilike :search", search: "%#{keyword.downcase}%")
+    where("title ilike :lsearch or description = :search or description ilike :lsearch", 
+      search: "%#{keyword}%")
   end
 
 end
