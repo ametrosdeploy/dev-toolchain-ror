99a3259d22d5b159b8edfac0beab890ef3653c71
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-12 01:55:52 +0530

    - COnflict

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,9 @@ Rails.application.routes.draw do
           end
         end
         resources :global_videos
-        resources :global_resources
+        constraints subdomain: ':type' do
+          resources :global_resources, path_prefix: ':resource_type'
+        end
       end
     end
   end
