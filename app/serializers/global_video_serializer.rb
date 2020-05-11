e8b16dcaba9a1290ea6c1deabd480f8d91f0964f
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-11 17:06:08 +0530

    - Resolve conflict

--- app/serializers/global_video_serializer.rb
+++ app/serializers/global_video_serializer.rb
@@ -1,4 +1,19 @@
 class GlobalVideoSerializer
   include FastJsonapi::ObjectSerializer
+  include ImageHelper
+  include DateHelper
   attributes :title, :description, :wistia_code, :duration, :video_type, :private, :transcript
+
+  attribute :wistia_thumbnail_url do |global_video|
+    image_url(global_video.wistia_thumbnail)
+  end
+
+  attribute :created_on do |global_video|
+    format_to_ymd(global_video.created_at)
+  end
+
+  attribute :customer do |global_video|
+    CustomerSerializer.new(global_video.customer).as_json["data"]
+  end
+
 end
