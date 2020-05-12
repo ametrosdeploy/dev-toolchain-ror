73f152f2dd7865903818b62e53cbacee29c0eae8
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-12 13:26:24 +0530

    - Conflict

--- app/models/organization_character.rb
+++ app/models/organization_character.rb
@@ -14,8 +14,6 @@ class OrganizationCharacter < ApplicationRecord
   belongs_to :organization
   belongs_to :world_role
 
-  validates_uniqueness_of :character_id, scope: [:organization_id, :world_role_id]
-
   scope :limited_org_chars, -> (car_list) { where("character_id not in (?)",
                                                                      car_list) }
 
