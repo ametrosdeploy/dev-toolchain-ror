068135a875156bc7030d2e59d9a26635fefb5f18
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-15 22:53:50 +0530

    - Content type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_111307) do
+ActiveRecord::Schema.define(version: 2020_05_15_172150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2020_05_15_111307) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "cached_tag_list"
+    t.integer "content_type"
     t.index ["customer_id"], name: "index_global_resources_on_customer_id"
   end
 
