f9cc770f3b848ce11c16045016ed7d4f35fc6b66
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-28 20:24:17 +0530

    - Bug fixes

--- config/spring.rb
+++ config/spring.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-if Rails.env == 'development' || Rails.env == 'test'
-  Spring.watch(
-    '.ruby-version',
-    '.rbenv-vars',
-    'tmp/restart.txt',
-    'tmp/caching-dev.txt'
-  )
-end
+# if Rails.env == 'development' || Rails.env == 'test'
+#   Spring.watch(
+#     '.ruby-version',
+#     '.rbenv-vars',
+#     'tmp/restart.txt',
+#     'tmp/caching-dev.txt'
+#   )
+# end
