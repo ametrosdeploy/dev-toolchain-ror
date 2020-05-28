ce4021a2ca4ec023b32ddf007fc69f4bd6e1bb75
sachin prasad <sachin.prasad@kiwitech.com>
2020-05-28 13:08:14 +0530

    - Sprint load issue

--- config/spring.rb
+++ config/spring.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Spring.watch(
-  '.ruby-version',
-  '.rbenv-vars',
-  'tmp/restart.txt',
-  'tmp/caching-dev.txt'
-)
+if Rails.env == 'development' || Rails.env == 'test'
+  Spring.watch(
+    '.ruby-version',
+    '.rbenv-vars',
+    'tmp/restart.txt',
+    'tmp/caching-dev.txt'
+  )
+end
