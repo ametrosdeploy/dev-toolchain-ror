2f2ff28973ca33eaedc32163a8ef8ed9cc41238d
nmathew12 <nmathew12@ametroslearning.com>
2020-05-29 14:00:05 -0400

    Rubocop changes

--- app/controllers/api/admin/v1/quiz_learn_objs_controller.rb
+++ app/controllers/api/admin/v1/quiz_learn_objs_controller.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 class Api::Admin::V1::QuizLearnObjsController < Api::Admin::V1::BaseController
 end
--- app/controllers/api/admin/v1/quiz_questions_controller.rb
+++ app/controllers/api/admin/v1/quiz_questions_controller.rb
@@ -4,7 +4,7 @@
 class Api::Admin::V1::QuizQuestionsController < Api::Admin::V1::BaseController
   before_action :authenticate_user!
   before_action :set_quiz_learn_obj
-  before_action :set_quiz_question, only: [:show, :update, :destroy]
+  before_action :set_quiz_question, only: %i[show update destroy]
 
   def index
     @quiz_questions = QuizQuestion.all
@@ -40,7 +40,7 @@ class Api::Admin::V1::QuizQuestionsController < Api::Admin::V1::BaseController
 
   swagger_controller :quiz_questions, 'Quiz Questions', resource_path:
      '/api/admin/v1/:quiz_learn_obj_id/quiz_questions'
-     
+
   swagger_api :create do
     summary 'Creates a Quiz Question'
     notes 'Should be used to create a quiz question'
@@ -49,42 +49,55 @@ class Api::Admin::V1::QuizQuestionsController < Api::Admin::V1::BaseController
     param :form, 'quiz_question[question]', :string, :required
     param :form, 'quiz_question[question_type]', :string, :required,
           'Options: "multiple_choice", "numeric", "range", "long_answer"'
-    param :form, 'quiz_question[points]', :integer, :required, 'Right answer Points'
-    param :form, 'quiz_question[feedback_from_watson]', :boolean, :optional, 'fetch feedback from watson'
-    param :form, 'quiz_question[mcq_options_attributes][]', :mcq_option_attr, :required, 'MCQ question options'
-    param :form, 'quiz_question[quiz_feedback_attributes][right_ans_feedback]', :string, :optional, 'Feedback if user answer is Right'
-    param :form, 'quiz_question[quiz_feedback_attributes][wrong_ans_feedback]', :string, :optional, 'Feedback if user answer is Wrong'
+    param :form, 'quiz_question[points]', :integer,
+          :required, 'Right answer Points'
+    param :form, 'quiz_question[feedback_from_watson]', :boolean,
+          :optional, 'fetch feedback from watson'
+    param :form, 'quiz_question[mcq_options_attributes][]', :mcq_option_attr,
+          :required, 'MCQ question options'
+    param :form, 'quiz_question[quiz_feedback_attributes]
+          [right_ans_feedback]', :string,
+          :optional, 'Feedback if user answer is Right'
+    param :form, 'quiz_question[quiz_feedback_attributes]
+          [wrong_ans_feedback]', :string,
+          :optional, 'Feedback if user answer is Wrong'
 
     response :unauthorized
-    response :not_acceptable
   end
 
   swagger_model :mcq_option_attr do
-    description "Option attributes for MCQ Questions"
-    property :order, :integer, :required, "Option order"
-    property :option, :string, :required, "Name"
-    property :is_correct, :boolean, :required, "Is correct answer?"
+    description 'Option attributes for MCQ Questions'
+    property :order, :integer, :required, 'Option order'
+    property :option, :string, :required, 'Name'
+    property :is_correct, :boolean, :required, 'Is correct answer?'
   end
-   
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_quiz_learn_obj
-      @quiz_learn_obj = QuizLearnObj.find(params[:quiz_learn_obj_id])
-    end
 
-    def set_quiz_question
-      @quiz_question = QuizQuestion.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quiz_learn_obj
+    @quiz_learn_obj = QuizLearnObj.find(params[:quiz_learn_obj_id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def quiz_question_params
-      params.require(:quiz_question).permit( :question_type, :question, :order, :points, :feedback_from_watson,  
-                                              mcq_options_attributes: [:order, :option, :is_correct, :_destroy],
-                                              quiz_feedback_attributes: [:right_ans_feedback, :wrong_ans_feedback, :_destroy] )
-    end
+  def set_quiz_question
+    @quiz_question = QuizQuestion.find(params[:id])
+  end
 
-    def serializer
-      QuizQuestionSerializer
-    end
+  # Only allow a trusted parameter "white list" through.
+  def quiz_question_params
+    params.require(:quiz_question).permit(:question_type, :question, :order,
+                                          :points, :feedback_from_watson,
+                                          mcq_options_attributes: %i[
+                                            order option
+                                            is_correct _destroy
+                                          ],
+                                          quiz_feedback_attributes: %i[
+                                            right_ans_feedback
+                                            wrong_ans_feedback _destroy
+                                          ])
+  end
+
+  def serializer
+    QuizQuestionSerializer
+  end
 end
