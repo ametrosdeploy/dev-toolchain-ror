c16c749892e713ae3f2bec46ec2a4317ccb2beb6
nmathew12 <nmathew12@ametroslearning.com>
2020-06-02 10:29:56 -0400

    quiz learn obj controller added

--- app/controllers/api/admin/v1/quiz_learn_objs_controller.rb
+++ app/controllers/api/admin/v1/quiz_learn_objs_controller.rb
@@ -1,4 +1,61 @@
 # frozen_string_literal: true
 
 class Api::Admin::V1::QuizLearnObjsController < Api::Admin::V1::BaseController
+  before_action :authenticate_user!
+  before_action :set_quiz_learn_obj, only: %i[show update destroy]
+
+  def show
+    render json: serialize_rec(@quiz_learn_obj)
+  end
+
+  def update
+    if @quiz_learn_obj.update(quiz_lo_params)
+      render json: serialize_rec(@quiz_learn_obj)
+    else
+      render json: @quiz_learn_obj.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quiz_learn_obj.destroy
+  end
+
+  swagger_controller :quiz_questions, 'Quiz Learning Object', resource_path:
+     '/api/admin/v1/quiz_learn_objs'
+     swagger_api :show do
+      summary 'Show Quiz Learning Object'
+      notes 'Should be used to show a quiz learning object'
+      param :header, :Authorization, :string, :required, 'Authorization'
+      param :path, 'id', :integer, :required, 'Quiz LO ID'
+    end
+  
+    swagger_api :update do
+      summary 'Update quiz learning object'
+      notes 'Should be used to update a quiz learning object'
+      param :header, :Authorization, :string, :required, 'Authorization'
+      param :path, 'id', :integer, :required, 'Quiz learning object ID'
+      param :form, 'quiz_learn_obj[title]', :string, :required
+      param :form, 'quiz_learn_obj[description]', :string, :required
+      param :form, 'quiz_learn_obj[score_view_type]', :string, :required,
+      'Options: "multiple_choice", "numeric", "percentage", "no_score"'
+    end
+  
+    swagger_api :destroy do
+      summary 'Destroy a quiz question'
+      notes 'Should be used to destroy a quiz question'
+      param :header, :Authorization, :string, :required, 'Authorization'
+      param :path, 'quiz_learn_obj_id', :integer, :required, 'Quiz LO ID'
+      param :path, 'id', :integer, :required, 'Quiz Question ID'
+    end
+
+    private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quiz_learn_obj
+    @quiz_learn_obj = QuizLearnObj.find(params[:quiz_learn_obj_id])
+  end
+
+  def quiz_lo_params
+  end
+
 end
