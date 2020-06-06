42c2c06ae2eab98e40867128b7b4aa63877f5186
nmathew12 <nmathew12@ametroslearning.com>
2020-06-06 15:13:34 -0400

    Asst entity models changed

--- app/models/asst_entity_value.rb
+++ app/models/asst_entity_value.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: asst_entity_values
+#
+#  id             :bigint           not null, primary key
+#  value          :string
+#  asst_entity_id :bigint           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class AsstEntityValue < ApplicationRecord
   belongs_to :asst_entity
 end
--- db/migrate/20200606185151_create_asst_entities.rb
+++ db/migrate/20200606185151_create_asst_entities.rb
@@ -2,7 +2,7 @@ class CreateAsstEntities < ActiveRecord::Migration[6.0]
   def change
     create_table :asst_entities do |t|
       t.string :name
-      t.references :entity_addable, polymorphic: true, null: false, index: { name: :entity_addable_index }
+      t.references :learning_object, null: false, foreign_key: true
 
       t.timestamps
     end
