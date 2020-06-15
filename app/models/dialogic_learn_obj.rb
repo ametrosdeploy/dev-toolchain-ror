0a9d50da7521b4def1a9b9ec9ab0a66af1d695df
nmathew12 <nmathew12@ametroslearning.com>
2020-06-15 14:18:56 -0400

    Dialogic  Lo Creation

--- app/models/dialogic_learn_obj.rb
+++ app/models/dialogic_learn_obj.rb
@@ -4,9 +4,24 @@
 #
 # Table name: dialogic_learn_objs
 #
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :bigint           not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  title              :string
+#  character_id       :integer
+#  repeat_interaction :boolean          default(FALSE)
+#  max_repeat_count   :integer
+#  unlimited_repeats  :boolean          default(FALSE)
 #
 class DialogicLearnObj < ApplicationRecord
+  # Associations ...
+  belongs_to :character, class_name: 'WorldOrgCharacter'
+  has_one :learning_object, as: :objectable
+  has_one :learn_mod, through: :learning_objects
+
+  # Validations ...
+  validates :title, presence: true
+  # validates :max_repeat_count,
+  #           numericality: { greater_than: 0 },
+  #           if: :max_repeat_count?
 end
