73b574c09d77a47e69098112ca3233f3ddcc95da
nmathew12 <nmathew12@ametroslearning.com>
2020-06-17 08:31:52 -0400

    Spec file path changed

--- spec/routing/api/admin/v1/dialogic_questions_routing_spec.rb
+++ spec/routing/api/admin/v1/dialogic_questions_routing_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe DialogicQuestionsController, type: :routing do
+RSpec.describe Api::Admin::V1::DialogicQuestionsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/dialogic_questions').to route_to('dialogic_questions#index')
