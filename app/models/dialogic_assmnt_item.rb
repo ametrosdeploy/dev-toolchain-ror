688706965a21725b945317f3e6f0caaaf4e99d9c
nmathew12 <nmathew12@ametroslearning.com>
2020-06-17 13:17:37 -0400

    Dialogic response Added

--- app/models/dialogic_assmnt_item.rb
+++ app/models/dialogic_assmnt_item.rb
@@ -20,8 +20,10 @@ class DialogicAssmntItem < ApplicationRecord
   belongs_to :assessment_label
   has_one :follow_up_question, dependent: :destroy
   has_many :required_key_topic_values, dependent: :destroy
+  has_many :dialogic_responses, dependent: :destroy
 
   # Nested Attributes ...
   accepts_nested_attributes_for :follow_up_question, allow_destroy: true
   accepts_nested_attributes_for :required_key_topic_values, allow_destroy: true
+  accepts_nested_attributes_for :dialogic_responses, allow_destroy: true
 end
