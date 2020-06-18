c505c4afa410a9a4285137c7117a72f27df4bc97
nmathew12 <nmathew12@ametroslearning.com>
2020-06-18 10:32:15 -0400

    Spec files : path corrected

--- spec/requests/api/admin/v1/assessment_schemes_spec.rb
+++ spec/requests/api/admin/v1/assessment_schemes_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'QuizQuestions', type: :request do
-  describe 'GET /quiz_questions' do
+RSpec.describe 'Api::Admin::V1::AssessmentSchemes', type: :request do
+  describe 'GET /assessment_schemes' do
     it 'works! (now write some real specs)' do
-      get quiz_questions_path
+      get assessment_schemes_path
       expect(response).to have_http_status(200)
     end
   end
--- spec/requests/api/admin/v1/quiz_questions_spec.rb
+++ spec/requests/api/admin/v1/quiz_questions_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'AssessmentSchemes', type: :request do
-  describe 'GET /assessment_schemes' do
+RSpec.describe 'Api::Admin::V1::QuizQuestions', type: :request do
+  describe 'GET /quiz_questions' do
     it 'works! (now write some real specs)' do
-      get assessment_schemes_path
+      get quiz_questions_path
       expect(response).to have_http_status(200)
     end
   end
