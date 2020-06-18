c0a6c28104a3b7c221475aeb7d6fa2f51abdcc54
nmathew12 <nmathew12@ametroslearning.com>
2020-06-18 10:32:15 -0400

    Spec files : path corrected

--- spec/requests/api/admin/v1/entity_evaluations_spec.rb
+++ spec/requests/api/admin/v1/entity_evaluations_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'OverallAssmntItems', type: :request do
-  describe 'GET /overall_assmnt_items' do
+RSpec.describe 'Api::Admin::V1::EntityEvaluations', type: :request do
+  describe 'GET /entity_evaluations' do
     it 'works! (now write some real specs)' do
-      get overall_assmnt_items_path
+      get entity_evaluations_path
       expect(response).to have_http_status(200)
     end
   end
