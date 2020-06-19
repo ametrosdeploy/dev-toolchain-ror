f5b75af9a51cd6588a28708d3a2ab364a4959011
sachin prasad <sachin.prasad@kiwitech.com>
2020-06-19 17:47:32 +0530

    - Quiz questions

---
+++ spec/requests/api/v1/quiz_questions_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::QuizQuestions', type: :request do
+  describe 'GET /api/v1/quiz_questions' do
+    it 'works! (now write some real specs)' do
+      get api_v1_quiz_questions_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
