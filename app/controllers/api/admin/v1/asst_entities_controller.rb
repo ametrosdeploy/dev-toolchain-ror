d6c8f35e7b3ba2da1c072308687e165cfbd28703
nmathew12 <nmathew12@ametroslearning.com>
2020-06-21 11:46:09 -0400

    Assistant entity value controller added to support shallow routes

--- app/controllers/api/admin/v1/asst_entities_controller.rb
+++ app/controllers/api/admin/v1/asst_entities_controller.rb
@@ -2,11 +2,13 @@
 
 # Controller for creating assistant entities ...
 class Api::Admin::V1::AsstEntitiesController < Api::Admin::V1::BaseController
-  before_action :set_learn_mod, :set_learning_object
+  before_action :set_learning_object, only: %i[index create]
+
   before_action :set_asst_entity, only: %i[show update destroy
                                            add_value_and_synonyms
                                            update_value_and_synonyms]
-  LEARN_MOD_ID = 'learn_mod ID'
+
+  before_action :set_entity_handler, only: %i[create update]
   LEARN_OBJ_ID = 'learning object ID'
 
   # GET /asst_entities
@@ -22,51 +24,36 @@ class Api::Admin::V1::AsstEntitiesController < Api::Admin::V1::BaseController
 
   # POST /asst_entities
   def create
-    hanlder = AsstElementHandler::Entity.new(create_hsh)
-    entity = hanlder.create_entity
-    if entity
-      render json: serialize_rec(entity), status: :created
+    @asst_entity = @learning_object.asst_entities
+                                   .new(asst_entity_params)
+
+    @entity_hanlder.create_entity
+    if @entity_hanlder.success? && @asst_entity.save
+      render json: serialize_rec(@asst_entity), status: :created
     else
-      render json: entity.errors, status: :unprocessable_entity
+      render json: { error: errors }, status: :unprocessable_entity
     end
   end
 
-  def add_value_and_synonyms
-    hanlder = AsstElementHandler::Entity.new(create_hsh)
-    entity = hanlder.create_entity_val_and_synonyms(value_param[:value],
-                                                    synonyms_param)
-    if entity
-      render json: serialize_rec(entity)
-    else
-      render json: entity.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update_value_and_synonyms
-    hanlder = AsstElementHandler::Entity.new(create_hsh)
-    entity = hanlder.update_val_and_synonyms(value_param[:value],
-                                             entity_new_val,
-                                             synonyms_param)
-    if entity
-      render json: serialize_rec(entity)
-    else
-      render json: entity.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   @entity_hanlder.update_entity()
+  #   if @entity_hanlder.success? && @asst_entity.save
+  #     render json: serialize_rec(@asst_entity), status: :created
+  #   else
+  #     render json: { error: errors }, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /asst_entities/1
   def destroy
     @asst_entity.destroy
   end
 
-  swagger_controller :asst_entities, 'AsstEntity', resource_path:
-     '/api/admin/v1/learn_mods/:learn_mod_id/learning_objects/:learning_object_id/asst_entities'
-
+  swagger_controller :asst_entities, 'Assistant Entity'
   swagger_api :index do
     summary 'List Asst Entities'
     notes 'Should be used to list Asst Entities'
     param :header, :Authorization, :string, :required, 'Authorization'
-    param :path, 'learn_mod_id', :integer, :required, LEARN_MOD_ID
     param :path, 'learning_object_id', :integer, :required, LEARN_OBJ_ID
   end
 
@@ -74,8 +61,6 @@ class Api::Admin::V1::AsstEntitiesController < Api::Admin::V1::BaseController
     summary 'Show Asst Entities'
     notes 'Should be used to show Asst Entities'
     param :header, :Authorization, :string, :required, 'Authorization'
-    param :path, 'learn_mod_id', :integer, :required, LEARN_MOD_ID
-    param :path, 'learning_object_id', :integer, :required, LEARN_OBJ_ID
     param :path, 'id', :integer, :required, 'Asst Entity id'
   end
 
@@ -83,42 +68,18 @@ class Api::Admin::V1::AsstEntitiesController < Api::Admin::V1::BaseController
     summary 'Creates an Assistant Entity'
     notes 'Should be used to create an Assistant Entity'
     param :header, :Authorization, :string, :required, 'Authorization'
-    param :path, 'learn_mod_id', :integer, :required, LEARN_MOD_ID
     param :path, 'learning_object_id', :integer, :required, LEARN_OBJ_ID
     param :form, 'asst_entity[name]', :string, :required, 'name'
   end
 
-  swagger_api :add_value_and_synonyms do
-    summary 'Adds value and synonyms to an Asst Entity'
-    notes 'Should be used to add value and synonyms to an Asst Entity'
-    param :header, :Authorization, :string, :required, 'Authorization'
-    param :path, 'learn_mod_id', :integer, :required, LEARN_MOD_ID
-    param :path, 'learning_object_id', :integer, :required, LEARN_OBJ_ID
-    param :path, 'id', :integer, :required, 'Asst Entity id'
-    param :form, 'asst_entity[asst_entity_values_attributes][value]',
-          :string, :required, 'name'
-    param :form,
-          'asst_entity[asst_entity_values_attributes]
-          [asst_entity_val_synonyms_attributes][synonym][]',
-          :string, :required, 'Synonyms'
-  end
-
-  swagger_api :update_value_and_synonyms do
-    summary 'Updates Asst Entity - value and synonyms '
-    notes 'Should be used to update value/synonyms of an Asst Entity'
-    param :header, :Authorization, :string, :required, 'Authorization'
-    param :path, 'learn_mod_id', :integer, :required, LEARN_MOD_ID
-    param :path, 'learning_object_id', :integer, :required, LEARN_OBJ_ID
-    param :path, 'id', :integer, :required, 'Asst Entity id'
-    param :form, 'asst_entity[asst_entity_values_attributes][value]',
-          :string, :required, 'value'
-    param :form, 'asst_entity[asst_entity_values_attributes][new_value]',
-          :string, :required, 'New value'
-    param :form,
-          'asst_entity[asst_entity_values_attributes]
-          [asst_entity_val_synonyms_attributes][synonym][]',
-          :string, :optional, 'Synonyms'
-  end
+  # swagger_api :update do
+  #   summary 'Updates assistant entity value'
+  #   notes 'Should be used to create an Assistant entity value'
+  #   param :header, :Authorization, :string, :required, 'Authorization'
+  #   param :path, 'learning_object_id', :integer, :required, LEARN_OBJ_ID
+  #   param :path, 'id', :integer, :required, 'ID'
+  #   param :form, 'asst_entity[name]', :string, :required, 'name'
+  # end
 
   private
 
@@ -130,44 +91,30 @@ class Api::Admin::V1::AsstEntitiesController < Api::Admin::V1::BaseController
     )
   end
 
-  def set_learn_mod
-    @learn_mod = LearnMod.find(params[:learn_mod_id])
-  end
-
   def set_asst_entity
     @asst_entity = AsstEntity.find(params[:id])
+    @learning_object ||= @asst_entity.learning_object
+  end
+
+  def set_entity_handler
+    @entity_hanlder = AsstElementHandler::Entity.new(create_hsh)
   end
 
   def create_hsh
     {
-      learn_mod: @learn_mod,
       learning_object: @learning_object,
       entity_name: asst_entity_params[:name],
       entity: @asst_entity
     }
   end
 
+  def errors
+    @entity_hanlder.full_messages || @asst_entity.errors
+  end
+
   # Only allow a trusted parameter "white list" through.
   def asst_entity_params
-    params.require(:asst_entity).permit(
-      :name,
-      asst_entity_values_attributes: [
-        :value,
-        asst_entity_val_synonyms_attributes: [synonym: []]
-      ]
-    )
-  end
-
-  def value_param
-    asst_entity_params[:asst_entity_values_attributes]
-  end
-
-  def entity_new_val
-    params[:asst_entity][:asst_entity_values_attributes][:new_value]
-  end
-
-  def synonyms_param
-    value_param[:asst_entity_val_synonyms_attributes][:synonym]
+    params.require(:asst_entity).permit(:name)
   end
 
   def serializer
