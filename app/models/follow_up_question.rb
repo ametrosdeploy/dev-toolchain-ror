6f448a61335ca35bf129c3462e14ee78bc52e7be
nmathew12 <nmathew12@ametroslearning.com>
2020-06-26 14:41:59 -0400

    Missed Assmnt item added

--- app/models/follow_up_question.rb
+++ app/models/follow_up_question.rb
@@ -4,14 +4,15 @@
 #
 # Table name: follow_up_questions
 #
-#  id                      :bigint           not null, primary key
-#  dialogic_assmnt_item_id :bigint           not null
-#  question                :text
-#  points                  :float
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
+#  id                  :bigint           not null, primary key
+#  question            :text
+#  points              :float
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  follow_up_able_type :string
+#  follow_up_able_id   :bigint
 #
 class FollowUpQuestion < ApplicationRecord
   # Associations ...
-  belongs_to :dialogic_assmnt_item
+  belongs_to :follow_up_able, polymorphic: true
 end
