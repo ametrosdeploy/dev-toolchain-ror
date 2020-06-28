d0e2e993f61e3e40e73e474fbfb801e9698998a9
nmathew12 <nmathew12@ametroslearning.com>
2020-06-28 11:54:44 -0400

    Key topic values reference to asst entity value

--- db/migrate/20200617121803_create_key_topic_values.rb
+++
@@ -1,9 +0,0 @@
-class CreateKeyTopicValues < ActiveRecord::Migration[6.0]
-  def change
-    create_table :key_topic_values do |t|
-      t.references :key_topic, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
