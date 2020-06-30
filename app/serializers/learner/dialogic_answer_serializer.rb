fbbe2326beb58183172455549169b018ccd0b8a6
nmathew12 <nmathew12@ametroslearning.com>
2020-06-30 13:19:11 -0400

    Responses and follow-up question after each question sent

--- app/serializers/learner/dialogic_answer_serializer.rb
+++ app/serializers/learner/dialogic_answer_serializer.rb
@@ -5,7 +5,8 @@ module Learner
     include FastJsonapi::ObjectSerializer
     attributes :dialogic_evaluation_id, :dialogic_question_id,
                :dialogic_question_id, :answer, :evaluated,
-               :follow_up_answer
+               :follow_up_answer, :character_response,
+               :follow_up_question
 
     attribute :key_topic_evaluations do |ans|
       AnswerKeyTopicEvaluationSerializer.new(ans.answer_key_topic_evaluations).as_json['data']
