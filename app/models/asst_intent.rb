96ef6c14ffcdcde1b20a2cd1123de97743ec4977
nmathew12 <nmathew12@ametroslearning.com>
2020-07-03 11:51:41 -0400

    Asst Intent APIs

--- app/models/asst_intent.rb
+++ app/models/asst_intent.rb
@@ -16,6 +16,13 @@ class AsstIntent < ApplicationRecord
   belongs_to :learning_object
   has_many :asst_intent_examples, dependent: :destroy
 
+  # Validations ...
+  validates :name, presence: true
+  validates_uniqueness_of :name, scope: :learning_object_id
+
+  # Nested Attributes ...
+  accepts_nested_attributes_for :asst_intent_examples, allow_destroy: true
+
   # Callbacks ...
   after_destroy :destroy_intent_from_watson
 
@@ -26,5 +33,25 @@ class AsstIntent < ApplicationRecord
                    intent_name: name }
     handler = AsstElementHandler::Intent.new(intent_hsh)
     handler.remove_intent
+  end
+
+  # Methods ...
+  def self.import(file, obj_id)
+    CSV.foreach(file.path, headers: false) do |row|
+      example = row[0]
+      intent = row[1]
+      # Have to find a way to add/ update intent & examples
+      intent = find_or_create_by(name: intent, learning_object_id: obj_id)
+      intent.asst_intent_examples.find_or_create_by(example: example)
+    end
+  end
+
+  def self.generate(intent, examples, obj_id)
+    intent = find_or_create_by(name: intent, learning_object_id: obj_id)
+    examples.each do |example|
+      intent.asst_intent_examples.find_or_create_by(
+        example: example
+      )
+    end
   end
 end
