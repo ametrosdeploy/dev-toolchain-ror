07c2746423cf3aa304777cbc2ef8dcf1c641a472
nmathew12 <nmathew12@ametroslearning.com>
2020-07-05 08:05:22 -0400

    NLU training inputs: Added

---
+++ app/serializers/nlu_training_input_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: nlu_training_inputs
+#
+#  id                 :bigint           not null, primary key
+#  message            :text
+#  email_learn_obj_id :bigint           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+class NluTrainingInputSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :message
+end
