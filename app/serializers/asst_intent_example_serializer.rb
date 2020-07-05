3a77957d4a97d89e047061a55dd26f4954b32c16
nmathew12 <nmathew12@ametroslearning.com>
2020-07-05 08:19:38 -0400

    asst entity annotated

--- app/serializers/asst_intent_example_serializer.rb
+++ app/serializers/asst_intent_example_serializer.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: asst_intent_examples
+#
+#  id             :bigint           not null, primary key
+#  example        :text
+#  asst_intent_id :bigint           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class AsstIntentExampleSerializer
   include FastJsonapi::ObjectSerializer
   attributes :example
