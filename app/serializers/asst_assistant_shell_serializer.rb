624906ddd4ca9500381bfb1595a2ac321637f281
Chris Caple <chriscaple@gmail.com>
2020-07-07 10:33:52 -0400

    Removes extraneous attributes from asst_assistant_shell_serializer.rb

--- app/serializers/asst_assistant_shell_serializer.rb
+++ app/serializers/asst_assistant_shell_serializer.rb
@@ -21,10 +21,6 @@ class AsstAssistantShellSerializer
     attributes :name, :assistant_dialog_skill_id, :guid, :url, :api_key, :credentials_name
   
   
-    attribute :created_on do |character|
-      format_to_ymd(character.created_at)
-    end
-  
-    attribute :gender, &:gender_name
+    
   end
   
