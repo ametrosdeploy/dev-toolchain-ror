661581b7bd16d7b186f2b75c49450b0c9cedd401
sachin prasad <sachin.prasad@kiwitech.com>
2020-07-10 14:30:55 +0530

    - server breaking

--- app/serializers/learner/mcq_option_detail_serializer.rb
+++ app/serializers/learner/mcq_option_detail_serializer.rb
@@ -4,6 +4,6 @@ module Learner
   # McqOption serializer for learner with is_correct
   class McqOptionDetailSerializer
     include FastJsonapi::ObjectSerializer
-    attributes attributes :order, :option, :is_correct
+    attributes :order, :option, :is_correct
   end
 end
