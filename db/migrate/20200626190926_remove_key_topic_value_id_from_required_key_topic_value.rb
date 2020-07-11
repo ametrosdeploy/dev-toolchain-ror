63ab38d1e6c696f7b95470a792b1cc81b1ad5535
sachin prasad <sachin.prasad@kiwitech.com>
2020-07-11 21:33:31 +0530

    - Stage migration issue

--- db/migrate/20200626190926_remove_key_topic_value_id_from_required_key_topic_value.rb
+++ db/migrate/20200626190926_remove_key_topic_value_id_from_required_key_topic_value.rb
@@ -1,5 +1,5 @@
 class RemoveKeyTopicValueIdFromRequiredKeyTopicValue < ActiveRecord::Migration[6.0]
   def change
-    add_column :required_key_topic_values, :key_topic_value_id, :integer
+    # add_column :required_key_topic_values, :key_topic_value_id, :integer
   end
 end
