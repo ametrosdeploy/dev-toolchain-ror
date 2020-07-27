344378f786863758dd10e059b4cf95de288b012d
Chris Caple <chriscaple@gmail.com>
2020-07-26 22:54:31 -0400

    Test chat session connection working

--- app/handler/asst_element_handler/base.rb
+++ app/handler/asst_element_handler/base.rb
@@ -9,6 +9,13 @@ module AsstElementHandler
       @learn_mod = args[:learn_mod] || @learning_object&.learn_mod
       guid = @learn_mod.asst_service_instance&.guid
       skill_id = @learning_object.assistant_dialog_skill&.skill_id
+      unless @learning_object.assistant_dialog_skill.blank?
+        unless @learning_object.assistant_dialog_skill.asst_assistant_shell.blank?
+          assistant_api_key = @learning_object.assistant_dialog_skill.asst_assistant_shell.api_key
+          assistant_id = @learning_object.assistant_dialog_skill.asst_assistant_shell.assistant_id
+          @assistant_v2_service = AssistantV2Service.new(assistant_api_key, assistant_id)
+        end
+      end
       @assistant_service = AssistantService.new(guid, skill_id)
       @errors = []
       @response = nil
---
+++ app/services/assistant_v2_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'ibm_watson/authenticators'
+require 'ibm_watson/assistant_v2'
+
+# Watson Assistant related requests
+class AssistantV2Service < BaseService
+  include IBMWatson
+
+  def initialize(assistant_api_key, assistant_id)
+    super()
+    base_url = ENV['ASST_SERVICE_BASE_URL'].chomp("/instances/")
+    @assistant_v2 = connect_assistant_v2(base_url, assistant_api_key)
+  end
+
+  def connect_assistant_v2(base_url, assistant_api_key)
+    authenticator = Authenticators::IamAuthenticator.new(
+      apikey: assistant_api_key
+    )
+    @assistant_v2 = AssistantV2.new(
+      version: '2020-04-01',
+      authenticator: authenticator
+    )
+    @assistant_v2.service_url = base_url
+    @assistant_v2
+  end
+
+
+  def create_assistant_session(assistant_id) 
+    @assistant_v2.create_session(
+      assistant_id: assistant_id
+    )
+  end
+end
