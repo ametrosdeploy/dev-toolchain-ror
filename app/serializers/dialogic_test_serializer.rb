1cb201b69dc9714a7f4904b246f3ff9c236eea5f
nmathew12 <nmathew12@ametroslearning.com>
2020-08-03 10:29:32 -0400

    Dialogic Test Model annotations

--- app/serializers/dialogic_test_serializer.rb
+++ app/serializers/dialogic_test_serializer.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: dialogic_tests
+#
+#  id                     :bigint           not null, primary key
+#  dialogic_learn_obj_id  :bigint           not null
+#  overall_assmnt_item_id :bigint
+#  overall_points         :float
+#  user_id                :bigint           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 # Dialogic Test Serializer ...
 class DialogicTestSerializer
   include FastJsonapi::ObjectSerializer
