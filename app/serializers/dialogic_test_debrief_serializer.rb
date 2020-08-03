8b4cbb3c294f9eeb642f1320c47932ec93b74bd0
nmathew12 <nmathew12@ametroslearning.com>
2020-08-03 10:29:32 -0400

    Dialogic Test Model annotations

--- app/serializers/dialogic_test_debrief_serializer.rb
+++ app/serializers/dialogic_test_debrief_serializer.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: dialogic_test_debriefs
+#
+#  id                  :bigint           not null, primary key
+#  dialogic_test_id    :bigint           not null
+#  key_topic_id        :bigint           not null
+#  assessment_label_id :bigint
+#  debrief_received    :text
+#  key_topic_missed    :boolean
+#  kt_points           :float
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
 # Serializer for Dialogic Test's debrief ...
 class DialogicTestDebriefSerializer
   include FastJsonapi::ObjectSerializer
--- app/serializers/dialogic_test_kt_eval_serializer.rb
+++ app/serializers/dialogic_test_kt_eval_serializer.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: dialogic_test_kt_evals
+#
+#  id                      :bigint           not null, primary key
+#  dialogic_test_answer_id :bigint           not null
+#  key_topic_id            :bigint           not null
+#  dialogic_assmnt_item_id :bigint
+#  missed_assmnt_item_id   :bigint
+#  points_earned           :float
+#  iteration_delivered     :integer
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#
 class DialogicTestKtEvalSerializer
   include FastJsonapi::ObjectSerializer
   attributes :key_topic_id, :points_earned
