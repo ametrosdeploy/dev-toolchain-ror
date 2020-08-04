3dd886021216520c3e534d06d87c6e44c6490c1a
Chris Caple <chriscaple@gmail.com>
2020-08-03 23:09:36 -0400

    Implements core chat logic

---
+++ db/migrate/20200804030501_add_columns_to_chat_evaluations.rb
@@ -0,0 +1,6 @@
+class AddColumnsToChatEvaluations < ActiveRecord::Migration[6.0]
+  def change
+    add_column :chat_evaluations, :skills_score_hash, :text
+    add_column :chat_evaluations, :skills_missed, :text
+  end
+end
