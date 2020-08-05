076ddcaf5535582e80b8a653f9791e77532530ee
nmathew12 <nmathew12@ametroslearning.com>
2020-08-05 08:06:10 -0400

    Adaptive content : Updates API added

--- app/controllers/api/admin/v1/adaptive_contents_controller.rb
+++ app/controllers/api/admin/v1/adaptive_contents_controller.rb
@@ -3,23 +3,19 @@
 # Here goes the code for Adaptive Content setup ...
 class Api::Admin::V1::AdaptiveContentsController < Api::Admin::V1::BaseController
   before_action :set_adaptable_class, only: %i[create]
-  before_action :set_adaptive_content, only: %i[destroy]
+  before_action :set_adaptive_content, only: %i[update destroy]
 
   CONTENT_TYPES = { video: 1, text: 2, slide: 3, file: 4 }
                   .with_indifferent_access.freeze
 
   def create
-    if content_type
-      content = AdaptiveContentHandler::CreateManager.for(create_hsh)
-      if content&.save_record
-        render json: content.response, status: 200
-      else
-        render json: content && content.errors || invalid_content, status: 422
-      end
-    else
-      render json: invalid_content, status: 422
-    end
+    create_or_update_adaptive_contents
   end
+
+  def update
+    create_or_update_adaptive_contents
+  end
+
 
   def destroy
     @adaptive_content.destroy
@@ -57,6 +53,34 @@ class Api::Admin::V1::AdaptiveContentsController < Api::Admin::V1::BaseControlle
     response :unauthorized
   end
 
+
+  swagger_api :update do
+    summary 'Updates an adaptive content'
+    notes 'Should be used to update an adaptive content'
+    param :header, :Authorization, :string, :required, 'Authorization'
+    param :path, 'id', :integer, :required, 'ID'
+    param :form, 'content_type', :string, :required, 'Options: "video",
+          "text", "slide", "file"'
+    param :form, 'adaptive_content[title]', :string, :optional, 'title'
+    param :form, 'adaptive_content[description]', :string, :optional,
+          'Description'
+    param :form, 'content[title]', :string, :optional, 'Rich Text'
+    param :form, 'content[global_resource_id]', :integer, :optional,
+          'global_resource_id'
+    param :form, 'content[global_video_id]', :integer, :optional,
+          'global_video_id'
+    param :form, 'content[slider_images_attributes][][id]', :integer,
+          :optional, 'slider_image id'
+    param :form, 'content[slider_images_attributes][][caption]', :string,
+          :optional, 'caption'
+    param :form, 'content[slider_images_attributes][][][global_resource_id]',
+          :integer, :optional, 'global_resource_id'
+    param :form, 'content[slider_images_attributes][][_destroy]', :string,
+          :optional, 'Set to true to remove slider_image'
+    param :form, 'content[has_caption]', :boolean, :optional, 'has_caption'
+    response :unauthorized
+  end
+
   swagger_api :destroy do
     summary 'Destroy an adaptive content'
     notes 'Should be used to destroy an adaptive content'
@@ -68,6 +92,19 @@ class Api::Admin::V1::AdaptiveContentsController < Api::Admin::V1::BaseControlle
 
   # Use callbacks to share common setup or constraints between actions.
 
+  def create_or_update_adaptive_contents
+    if content_type
+      content = AdaptiveContentHandler::CreateManager.for(create_hsh)
+      if content&.save_record
+        render json: content.response, status: 200
+      else
+        render json: content && content.errors || invalid_content, status: 422
+      end
+    else
+      render json: invalid_content, status: 422
+    end
+  end
+
   def set_adaptable_class
     @klass = params[:adaptable_type].constantize
     @adaptable = @klass.find(params[:adaptable_id])
@@ -75,6 +112,7 @@ class Api::Admin::V1::AdaptiveContentsController < Api::Admin::V1::BaseControlle
 
   def set_adaptive_content
     @adaptive_content = AdaptiveContent.find(params[:id])
+    @adaptable = @adaptive_content.adaptable
   end
 
   def adaptive_content_params
@@ -87,7 +125,7 @@ class Api::Admin::V1::AdaptiveContentsController < Api::Admin::V1::BaseControlle
       params: params,
       adaptable_obj: @adaptable,
       adaptive_content_params: adaptive_content_params,
-      content: @adaptable.adaptive_contents.build
+      content: @adaptive_content||@adaptable.adaptive_contents.build
     }
   end
 
