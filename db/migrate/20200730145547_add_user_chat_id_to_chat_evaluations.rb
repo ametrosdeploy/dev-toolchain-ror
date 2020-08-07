3c4a3834efde1fae7819912ecb5ff59fdd6e5093
Sachin Prasad <sachin.prasad@kiwitech.com>
2020-08-07 14:14:16 +0530

    - Commenting migrations related to Chat evaluations

--- db/migrate/20200730145547_add_user_chat_id_to_chat_evaluations.rb
+++ db/migrate/20200730145547_add_user_chat_id_to_chat_evaluations.rb
@@ -1,5 +1,5 @@
 class AddUserChatIdToChatEvaluations < ActiveRecord::Migration[6.0]
   def change
-    add_column :chat_evaluations, :user_chat_id, :bigint
+    # add_column :chat_evaluations, :user_chat_id, :bigint
   end
 end
