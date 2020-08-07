b7e4386a8c6b0db5e42b0849256dd072e4264909
Chris Caple <chriscaple@gmail.com>
2020-08-07 09:44:59 -0400

    Adds label name and order to EmailAssessmentItem JSON

--- app/serializers/email_assessment_item_serializer.rb
+++ app/serializers/email_assessment_item_serializer.rb
@@ -12,12 +12,13 @@
 #
 class EmailAssessmentItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :email_skill_id, :assessment_label_id
+  attributes :email_skill_id, :assessment_label_id, :assessment_label_name, :assessment_label_order
 
-  
+
 
   attribute :assessment_formulas do |assess|
     AssessmentFormulaSerializer.new(assess.assessment_formulas)
                              .as_json['data']
   end
+
 end
