9d02792dccbe57c5f7c49a76359a2fb47cbcbd72
nmathew12 <nmathew12@ametroslearning.com>
2020-08-10 15:45:14 -0400

    field for Email iteration required or not

--- app/serializers/learner/user_email_iteration_serializer.rb
+++ app/serializers/learner/user_email_iteration_serializer.rb
@@ -3,7 +3,9 @@
 module Learner
   class UserEmailIterationSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :email, :iteration, :qa_condition_hit, :user_email_evaluation, :overall_assmnt_item
+    attributes :email, :iteration, :qa_condition_hit,
+               :user_email_evaluation, :overall_assmnt_item,
+               :next_iteration_required
 
     attribute :responses do |rec|
       UserEmailIterationResponseSerializer.new(rec.user_email_iteration_responses).as_json['data']
