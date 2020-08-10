ddb887697cc9aaae7a377793ff69e84dab4f2f9e
Chris Caple <chriscaple@gmail.com>
2020-08-10 13:20:35 -0400

    Adds qa_condition_hit boolean to user_email_iterations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_135721) do
+ActiveRecord::Schema.define(version: 2020_08_10_165746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,7 +169,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_135721) do
   end
 
   create_table "asst_assistant_shells", force: :cascade do |t|
-    t.integer "assistant_dialog_skill_id"
     t.string "name"
     t.string "assistant_id"
     t.text "url"
@@ -177,7 +176,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_135721) do
     t.string "credentials_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["assistant_dialog_skill_id"], name: "index_asst_assistant_shells_on_assistant_dialog_skill_id"
+    t.integer "assistant_dialog_skill_id"
   end
 
   create_table "asst_entities", force: :cascade do |t|
@@ -717,13 +716,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_135721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["jti"], name: "index_jwt_blacklists_on_jti"
-  end
-
-  create_table "key_topic_values", force: :cascade do |t|
-    t.bigint "key_topic_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["key_topic_id"], name: "index_key_topic_values_on_key_topic_id"
   end
 
   create_table "key_topics", force: :cascade do |t|
@@ -1476,6 +1468,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_135721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "overall_assmnt_item_id"
+    t.boolean "qa_condition_hit", default: false, null: false
     t.index ["user_email_evaluation_id"], name: "index_user_email_iterations_on_user_email_evaluation_id"
   end
 
@@ -1671,7 +1664,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_135721) do
   add_foreign_key "global_resources", "customers"
   add_foreign_key "global_videos", "customers"
   add_foreign_key "interstitial_contents", "email_learn_objs"
-  add_foreign_key "key_topic_values", "key_topics"
   add_foreign_key "key_topics", "asst_entities"
   add_foreign_key "key_topics", "dialogic_questions"
   add_foreign_key "learn_mod_contributors", "learn_mod_contributor_roles"
