d5563696c9a1d442e2984c700ea8432a91a1efc1
Chris Caple <chriscaple@gmail.com>
2020-08-10 10:15:50 -0400

    Sets up email learner debrief APIs

--- app/models/user_email_assessment_item.rb
+++ app/models/user_email_assessment_item.rb
@@ -14,4 +14,18 @@
 class UserEmailAssessmentItem < ApplicationRecord
   belongs_to  :email_assessment_item 
   belongs_to  :user_email_evaluation
+
+  def email_skill 
+    self.email_assessment_item.email_skill 
+  end
+
+  def assessment_label 
+    self.email_assessment_item.assessment_label
+  end
+
+  def global_skill 
+    self.email_assessment_item.email_skill&.global_skill 
+  end
+
+  
 end
