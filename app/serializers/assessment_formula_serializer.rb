ff651c5d454f0d2c792b5addf66bde9b3aa8784e
nmathew12 <nmathew12@ametroslearning.com>
2020-08-11 08:38:55 -0400

    Assessment formula serializer issue

--- app/serializers/assessment_formula_serializer.rb
+++ app/serializers/assessment_formula_serializer.rb
@@ -22,49 +22,50 @@ class AssessmentFormulaSerializer
   attribute :assessment_formula_nlu_entities, if: proc { |record|
     record.assessment_formula_nlu_entities.present?
   } do |formula|
-    AssessmentFormulaNluEntitySerializer.new(formula.assessment_formula_nlu_entities)
+    FormulaNluEntitySerializer.new(formula.assessment_formula_nlu_entities)
                               .as_json['data']
   end
 
   attribute :assessment_formula_nlu_keywords, if: proc { |record|
     record.assessment_formula_nlu_keywords.present?
   } do |formula|
-    AssessmentFormulaNluKeywordSerializer.new(formula.assessment_formula_nlu_keywords)
+    FormulaNluKeywordSerializer.new(formula.assessment_formula_nlu_keywords)
                                .as_json['data']
   end
 
   attribute :assessment_formula_nlu_concepts, if: proc { |record|
     record.assessment_formula_nlu_concepts.present?
   } do |formula|
-    AssessmentFormulaNluConceptSerializer.new(formula.assessment_formula_nlu_concepts)
+    FormulaNluConceptSerializer.new(formula.assessment_formula_nlu_concepts)
                                .as_json['data']
   end
 
   attribute :assessment_formula_asst_intents, if: proc { |record|
     record.assessment_formula_asst_intents.present?
   } do |formula|
-    AssessmentFormulaAsstIntentSerializer.new(formula.assessment_formula_asst_intents)
+    FormulaAsstIntentSerializer.new(formula.assessment_formula_asst_intents)
                                .as_json['data']
   end
 
   attribute :assessment_formula_asst_entity_values, if: proc { |record|
     record.assessment_formula_asst_entity_values.present?
   } do |formula|
-    AssessmentFormulaAsstEntityValueSerializer.new(formula.assessment_formula_asst_entity_values)
-                                    .as_json['data']
+    FormulaAsstEntityValueSerializer.new(
+      formula.assessment_formula_asst_entity_values
+    ).as_json['data']
   end
 
   attribute :assessment_formula_emotions, if: proc { |record|
     record.assessment_formula_emotions.present?
   } do |formula|
-    AssessmentFormulaEmotionSerializer.new(formula.assessment_formula_emotions)
+    FormulaEmotionSerializer.new(formula.assessment_formula_emotions)
                             .as_json['data']
   end
 
   attribute :assessment_formula_sentiments, if: proc { |record|
     record.assessment_formula_sentiments.present?
   } do |formula|
-    AssessmentFormulaSentimentSerializer.new(formula.assessment_formula_sentiments)
+    FormulaSentimentSerializer.new(formula.assessment_formula_sentiments)
                               .as_json['data']
   end
 end
