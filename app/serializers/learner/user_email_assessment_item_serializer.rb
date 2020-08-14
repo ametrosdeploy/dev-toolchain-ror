0a6ad8cecd0a6412a06e6ef38804875412cd5338
Sachin Prasad <sachin.prasad@kiwitech.com>
2020-08-14 19:15:03 +0530

    - send assessment label detail

--- app/serializers/learner/user_email_assessment_item_serializer.rb
+++ app/serializers/learner/user_email_assessment_item_serializer.rb
@@ -3,10 +3,17 @@
 module Learner
   class UserEmailAssessmentItemSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :email_skill, :global_skill, :assessment_label, :debrief_content,
-               :debrief_variant, :debrief_id, :email_assessment_item,
-               :user_email_evaluation_id
+    include ImageHelper
+    attributes :email_skill, :global_skill, :debrief_content, :debrief_variant,
+               :debrief_id, :email_assessment_item, :user_email_evaluation_id
 
+    attribute :assessment_label do |assessment|
+      assessment.assessment_label.try(:name)
+    end
+
+    attribute :assessment_icon_url do |assessment|
+      image_url(assessment.assessment_label.try(:icon))
+    end
   end
 end
 
