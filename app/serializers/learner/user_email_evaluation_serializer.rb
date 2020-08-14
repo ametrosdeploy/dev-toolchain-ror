076cb1a71e4a08b612bef4a19dda5513b77135be
Sachin Prasad <sachin.prasad@kiwitech.com>
2020-08-14 11:32:20 +0530

    - User email evaluation changes

--- app/serializers/learner/user_email_evaluation_serializer.rb
+++ app/serializers/learner/user_email_evaluation_serializer.rb
@@ -5,8 +5,13 @@ module Learner
     include FastJsonapi::ObjectSerializer
     attributes :email_subject, :qa_condition_hit, :overall_score, :has_max_score
 
-    attribute :user_email_iterations do |eval|
-      UserEmailIterationSerializer.new(eval.user_email_iterations).as_json['data']
+    # attribute :user_email_iterations do |rec|
+    #   UserEmailIterationSerializer.new(rec.user_email_iterations).as_json['data']
+    # end
+
+    attribute :user_email_assessment_items do |rec|
+      assessment = rec.user_email_assessment_items
+      Learner::UserEmailAssessmentItemSerializer.new(assessment).as_json['data']
     end
   end
 end
