7cfce53c03179c90207563ee0bec93b9ea5bf7bd
Sachin Prasad <sachin.prasad@kiwitech.com>
2020-08-17 13:23:09 +0530

    - S3 upload changes

--- app/services/generate_final_eval_service.rb
+++ app/services/generate_final_eval_service.rb
@@ -1,4 +1,5 @@
 class GenerateFinalEvalService < BaseService
+  require 'aws-sdk-s3'
   include WickedPdf::WickedPdfHelper::Assets
   include ActionView::Helpers::AssetTagHelper
   include ActionView::Helpers::NumberHelper
@@ -13,18 +14,23 @@ class GenerateFinalEvalService < BaseService
     content                    = File.read('templates/final_evaluation.html.erb')
     template                   = ERB.new(content)
     pdf                        = WickedPdf.new.pdf_from_string(template.result(binding), show_as_html: true)
-    save_path = Rails.root.join('tmp',"filename-#{@user_section.id}.pdf")
+    file_name = "filename-#{@user_section.id}.pdf"
+    save_path = Rails.root.join('tmp', file_name)
     File.open(save_path, 'wb') do |file|
       file << pdf
     end
-    upload_to_s3(invoice_pdf, save_path)
+    upload_to_s3(file_name, save_path)
   end
 
-  def upload_to_s3(pdf_file, file_path)
+  def upload_to_s3(file_name, file_path)
     begin
-      s3 = Aws::S3::Resource.new(access_key_id: ENV['IBM_ACCESS_KEY'], secret_access_key: ENV['IBM_SECRET'], region: ENV['IBM_REGION'])
-      obj = s3.bucket('bucket-name').object('final_evaluation')
+      s3 = Aws::S3::Resource.new(access_key_id: ENV['IBM_ACCESS_KEY'],
+                                 secret_access_key: ENV['IBM_SECRET'],
+                                 region: ENV['IBM_REGION'],
+                                 endpoint: ENV['IBM_ENDPOINT'] )
+      obj = s3.bucket(ENV['IBM_BUCKET']).object('final_evaluation')
       obj.upload_file(file_path)
+
     rescue Exception => e
       raise e
     end
